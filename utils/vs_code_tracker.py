5c67096865e6d189a39335d4984115dc47e54c77
metekaya <metekaya55@gmail.com>
2023-11-02 12:53:45 +0300

    add logs to track issues

--- utils/vs_code_tracker.py
+++ utils/vs_code_tracker.py
@@ -2,20 +2,27 @@ import psutil
 import time
 from win10toast import ToastNotifier
 
+
 def track_vs_code_usage(threshold=30):
     toaster = ToastNotifier()
     while True:
         print("TRACKING VS CODE SCREENTIME")
-        active_process_names = [p.name() for p in psutil.process_iter(['name'])]
+        active_process_names = [p.name()
+                                for p in psutil.process_iter(['name'])]
         # print(active_process_names)
         if 'Code.exe' in active_process_names:
             start_time = time.time()
+            print("Timer Started..")
             while 'Code.exe' in active_process_names:
-                time.sleep(30) # check every 30 seconds
-                active_process_names = [p.name() for p in psutil.process_iter(['name'])]
+                time.sleep(15)  # check every 15 seconds
+                active_process_names = [p.name()
+                                        for p in psutil.process_iter(['name'])]
             end_time = time.time()
+            print("VS Code Closed, timer stopped")
             elapsed_minutes = (end_time - start_time) / 60
+            print("Elapsed time is: ", elapsed_minutes)
             if elapsed_minutes < threshold:
                 message = f"You've been using vs code for {elapsed_minutes:.2f} minutes. You need to study more!"
-                toaster.show_toast("VS Code Usage Notifier", message, duration=10)
-        time.sleep(30) # check every 30 seconds
+                toaster.show_toast("VS Code Usage Notifier",
+                                   message, duration=10)
+        time.sleep(15)  # check every 15 seconds
